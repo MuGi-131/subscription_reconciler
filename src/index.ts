b4bd9a7965acd25c29a1e09a88947bfd57998e68
MuGi-131 <munkhbayar.mugi@gmail.com>
2026-05-28 19:31:23 +0900

    chore: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+console.log("Environment variables loaded.");
+console.log("PORT:", PORT);
