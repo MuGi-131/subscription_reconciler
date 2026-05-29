543200752a856f80ca70c64e14e831cafeac0c6b
MuGi-131 <munkhbayar.mugi@gmail.com>
2026-05-29 12:21:21 +0900

    chore: add template and change in tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import dotenv from "dotenv";
+import app from "./app";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-console.log("Environment variables loaded.");
-console.log("PORT:", PORT);
+async function main() {
+  // TODO: db, worker, app load
+  app.listen(PORT, () => {
+    console.log(`Reconciler service running on port ${PORT}`);
+  });
+}
+
+main().catch((err) => {
+  console.log("Failed to start: ", err);
+  process.exit(1);
+});
